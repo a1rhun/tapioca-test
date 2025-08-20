c3b26ef3d50cb8efa4fecadbf8e64eb9a24b9a66
a1rhun <83606547+a1rhun@users.noreply.github.com>
2025-08-21 01:39:47 +0900

    UserDTO 생성

---
+++ src/main/java/com/example/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package com.example.dto;
+
+public class UserDTO {
+    private Integer u_id;
+    private String u_name;
+
+    // Getters and Setters
+    public Integer getU_id() {
+        return u_id;
+    }
+
+    public void setU_id(Integer u_id) {
+        this.u_id = u_id;
+    }
+
+    public String getU_name() {
+        return u_name;
+    }
+
+    public void setU_name(String u_name) {
+        this.u_name = u_name;
+    }
+}
