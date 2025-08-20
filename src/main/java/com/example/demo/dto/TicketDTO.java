c61c940d8b1aded9b6a3038fd5ab7c263de985a0
a1rhun <83606547+a1rhun@users.noreply.github.com>
2025-08-21 01:58:41 +0900

    TicketDTO 생성

---
+++ src/main/java/com/example/demo/dto/TicketDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+public class TicketDTO {
+    private Integer tId;
+    private String tName;
+
+    // Getters and Setters
+    public Integer getTId() {
+        return tId;
+    }
+
+    public void setTId(Integer tId) {
+        this.tId = tId;
+    }
+
+    public String getTName() {
+        return tName;
+    }
+
+    public void setTName(String tName) {
+        this.tName = tName;
+    }
+}
