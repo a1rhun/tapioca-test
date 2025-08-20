e94c5dbb7a22a26d19a0f467c559a54d625cbfb1
a1rhun <83606547+a1rhun@users.noreply.github.com>
2025-08-20 23:57:34 +0900

    Ticket DTO 생성

---
+++ dtos/TicketDTO.java
@@ -0,0 +1,31 @@
+package dtos;
+
+public class TicketDTO {
+    private Integer ticketId;
+    private Integer ticketUser;
+    private String ticketName;
+
+    public Integer getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Integer ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public Integer getTicketUser() {
+        return ticketUser;
+    }
+
+    public void setTicketUser(Integer ticketUser) {
+        this.ticketUser = ticketUser;
+    }
+
+    public String getTicketName() {
+        return ticketName;
+    }
+
+    public void setTicketName(String ticketName) {
+        this.ticketName = ticketName;
+    }
+}
