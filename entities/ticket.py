e98571feba8803ad73dba7f486052a096c8ea74a
a1rhun <83606547+a1rhun@users.noreply.github.com>
2025-08-20 23:56:11 +0900

    Delete entities directory

--- entities/ticket.py
+++
@@ -1,5 +0,0 @@
-class Ticket:
-    def __init__(self, ticket_id, ticket_name, ticket_user):
-        self.ticket_id = ticket_id
-        self.ticket_name = ticket_name
-        self.ticket_user = ticket_user
